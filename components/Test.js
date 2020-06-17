cbac4e57c4648f0f03cad06440357d7c28e34660
Maxime Smolis <mpsmolis@gmail.com>
2020-06-18 00:19:24 +0200

    first component created

---
+++ components/Test.js
@@ -0,0 +1,8 @@
+
+class Welcome extends React.Component {
+  render() {
+    return <h1>Bonjour Next.js</h1>; 
+  }
+}
+
+export default Welcome
---
+++ pages/index.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+import Test from '../components/Test'
+
+export default class extends Component {
+  render () {
+    return (
+      <div>
+        < Test />
+      </div>
+      
+    )
+  }
+}
